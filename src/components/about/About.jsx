2fb433de690e57685443cce52a7f6c1c7e371445
Gavrilo Stankovic <stakovic.gavrilo@gmail.com>
2023-07-26 02:59:48 +0200

    My Portfolio React App

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
       <div className="container about_container">
         <div className="about_me">
           <div className="about_me-image">
-            <img src={ME} alt="" />
+            <img src={ME} alt="me" />
           </div>
         </div>
         <div className="about_content">
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,13 +6,13 @@ import { FiDribbble } from "react-icons/fi";
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
-      <a href="https://linkedin.com" target="_blank">
+      <a href="https://linkedin.com" target="_blank" rel="noreferrer">
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank">
+      <a href="https://github.com" target="_blank" rel="noreferrer">
         <FaGithub />
       </a>
-      <a href="https://dribble.com" target="_blank">
+      <a href="https://dribble.com" target="_blank" rel="noreferrer">
         <FiDribbble />
       </a>
     </div>
